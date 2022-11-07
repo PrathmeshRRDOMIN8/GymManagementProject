79094b506cc47f342a32e91a8a068b413502cfec
Prathmesh Sinha <77191932+PrathmeshRRDOMIN8@users.noreply.github.com>
2022-11-07 18:50:47 +0530

    first commit1

---
+++ src/main/java/com/mycompany/gymsdtfisac2/GymSdtFisac2.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.gymsdtfisac2;
+/**
+ *
+ * @author prtme
+ */
+//import com.mongodb.client.MongoClients;
+//import com.mongodb.client.MongoClient;
+//import com.mongodb.client.MongoCollection;
+//import com.mongodb.client.MongoCursor;
+//import com.mongodb.client.MongoDatabase;
+public class GymSdtFisac2 {
+
+    public static void main(String[] args) {
+//      MongoClient Mongoclient = MongoClients.create("mongodb+srv://prathmeshDronaid:prathmeshDronaid1234@cluster0.kuv2win.mongodb.net/?retryWrites=true&w=majority");
+//      MongoDatabase db = Mongoclient.getDatabase("SampleDB");
+//      MongoCollection col = db.getCollection("SampleCollection");
+////      Document sampleDoc = new Document("_id","1").append("name","John Smith");
+////      col.insertOne(sampleDoc); 
+//      MongoCursor<String> dbcursor = Mongoclient.listDatabaseNames().iterator();
+//      while(dbcursor.hasNext()){
+//          System.out.print(dbcursor.next());
+//      }
+//         
+    }
+}
